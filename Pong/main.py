4b2125d63cd6fe94094eb2bee25b46def53f1f7d
stephanieliv <95098084+stephanieliv@users.noreply.github.com>
2023-08-06 09:04:34 +1000

    Create main.py

---
+++ Pong/main.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+scoreboard = Scoreboard()
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < - 320:
+        ball.bounce_x()
+        ball.move_speed *= 0.1
+
+
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.l_point()
+        #add points to left
+
+    if ball.xcor() < - 380:
+        ball.reset()
+        scoreboard.r_point()
+        #add points to right
+
+screen.exitonclick()
